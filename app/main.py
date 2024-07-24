5dfb9fdae7897ff6c4ba046e86612db2b44c0255
Pasha <pavik.varganov@gmail.com>
2024-07-24 14:44:43 +0300

    Solution 2 corrected the code according to comments

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
-def limitation(num: float, min_lim: float, max_lim: float) -> float:
+from __future__ import annotations
+
+
+def limitation(
+        num: int | float,
+        min_lim: int | float,
+        max_lim: int | float) -> int | float:
     if num < min_lim:
         return min_lim
     elif num > max_lim:
@@ -7,21 +13,23 @@ def limitation(num: float, min_lim: float, max_lim: float) -> float:
 
 
 class Car:
-    def __init__(self,
-                 comfort_class: int,
-                 clean_mark: int,
-                 brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str) -> None:
         self.comfort_class = limitation(comfort_class, 1, 7)
         self.clean_mark = limitation(clean_mark, 1, 10)
         self.brand = brand
 
 
 class CarWashStation:
-    def __init__(self,
-                 distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int) -> None:
         self.distance_from_city_center = limitation(
             distance_from_city_center, 1.0, 10.0
         )
@@ -38,14 +46,17 @@ class CarWashStation:
         return income
 
     def calculate_washing_price(self, car: Car) -> float:
-        return round(car.comfort_class * (self.clean_power - car.clean_mark)
-                     * self.average_rating / self.distance_from_city_center, 1)
+        return round(
+            car.comfort_class * (self.clean_power - car.clean_mark)
+            * self.average_rating / self.distance_from_city_center, 1)
 
     def wash_single_car(self, car: Car) -> None:
         car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> None:
-        self.average_rating = round((self.average_rating
-                                     * self.count_of_ratings + rate)
-                                    / (self.count_of_ratings + 1), 1)
+        self.average_rating = round(
+            (self.average_rating
+             * self.count_of_ratings + rate)
+            / (self.count_of_ratings + 1), 1
+        )
         self.count_of_ratings += 1
